27b7db3f6f5d2bb836213981633a7638a3bd15b6
Aliyar F <aliyarfatemi@gmail.com>
2018-04-24 20:09:23 +0000

    revision for roadmap

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,39 +4,50 @@ require_relative "roadmap"
 class Kele
     include HTTParty
     include Roadmap
+    base_uri 'https://www.bloc.io/api/v1'
      
     def initialize(email,password)
-        @api_url = "https://www.bloc.io/api/v1"
         @auth_token = get_auth_token(email,password)
-      
     end
     
     def get_auth_token(email,pw)
         response = Kele.post(
-            @api_url + "/sessions",
+            base_uri + "/sessions",
             body: {email: email, password: pw}
         )
         response["auth_token"]
     end
     
-
-   def get_me
-       response = JSON.parse(Kele.get(@api_url+"/users/me", headers: { "authorization" => @auth_token}).body)
-   end 
-   
-   def get_mentor_availability(mentor_id)
-       response = JSON.parse(Kele.get(@api_url+"/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token}).body)
-   end 
-   
-   
-
-    def get_messages
-    response = JSON.parse(Kele.get(@api_url+"/message_threads", headers: { "authorization" => @auth_token}).body)
+    def get_me
+        response = JSON.parse(Kele.get(
+        base_uri + "/users/me", headers: {
+        "authorization" => @auth_token
+         }).body)
     end
 
-    def create_message(recipient_id, message,email)
-    response = Kele.post(@api_url+"/messages", headers: { "authorization" => @auth_token} , body: { "sender" => email, "recipient_id" => recipient_id, "stripped-text" => message})
-    end
+     def get_mentor_availability(mentor_id)
+         response = JSON.parse(Kele.get(
+         base_uri + "/mentors/#{mentor_id}/student_availability", headers: {
+         "authorization" => @auth_token
+         }).body)
+     end
 
-   
+
+
+      def get_messages
+          response = JSON.parse(Kele.get(@api_url + "/message_threads", headers: {
+          "authorization" => @auth_token
+          }).body)
+      end
+
+      def create_message(recipient_id, message, email)
+          response = Kele.post(@api_url + "/messages", headers: {
+          "authorization" => @auth_token
+           }, body: {
+            "sender" => email,
+            "recipient_id" => recipient_id,
+            "stripped-text" => message
+            })
+       end
+    
 end 
--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,9 +1,22 @@
 module Roadmap
-    def get_roadmap(roadmap_id)
-      response = JSON.parse(Kele.get(@api_url+ "/roadmaps/#{roadmap_id}", headers: { "authorization" => @auth_token}).body)
-    end
+   
+   def get_roadmap(roadmap_id)
+       response = Kele.get(
+       base_uri + "/roadmaps/#{roadmap_id}",
+       headers: { "authorization" => @auth_token}
+     )
+
+      JSON.parse(response.body)
+      
+   end
   
     def get_checkpoint(checkpoint_id)
-      response = Kele.get(@api_url+ "/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token})
+       response = Kele.get(
+       base_uri + "/checkpoints/#{checkpoint_id}", 
+       headers: { "authorization" => @auth_token}
+       )
+      
+       JSON.parse(response.body)
+       
     end
 end
